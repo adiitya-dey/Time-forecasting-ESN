73f9e13216ff86fe02c093eeb8ac2596fc5ec0e2
aditya dey <adiitya.dey@gmail.com>
2024-05-28 15:12:33 +0200

    ESN is working successfully with and without Teacher_forcing. It cannot manage torch.ones.

---
+++ scripts/data/data_loader.py
@@ -0,0 +1,137 @@
+
+from darts.datasets import AirPassengersDataset, AusBeerDataset, ETTh1Dataset, ETTh2Dataset, ETTm1Dataset, ETTm2Dataset, ExchangeRateDataset, TrafficDataset
+
+import numpy as np
+import torch
+from sklearn.preprocessing import MinMaxScaler
+from einops import rearrange
+from sklearn.model_selection import train_test_split
+
+
+class AugmentedDataset:
+
+    def __init__(self):
+        self.points = np.linspace(1,100, 500)
+        self.noise = np.random.randn(len(self.points))
+
+    def __call__(self, data_name):
+        data_dict = {"linear": self.points,
+                     "seasonal": self.seasonal(self.points),
+                     "noise": self.noise,
+                     "linear seasonal": self.linear_seasonal(self.points),
+                     "linear noise": self.points + self.noise,
+                     "seasonal noise": self.seasonal(self.points) + self.noise,
+                     "linear seasonal noise": self.linear_seasonal(self.points) + self.noise
+
+        }
+        return data_dict[data_name]
+
+            
+
+
+    def seasonal(self,x):
+        return np.sin(2 * np.pi * 10 * x)
+    
+    def linear_seasonal(self,x, frequency=10, amplitude=1, trend_slope=0.1):
+        
+        trend = trend_slope * x
+        oscillations = amplitude * np.cos(2 * np.pi * frequency * x)
+        y = trend + oscillations
+        return y
+    
+
+
+class DartsDataset:
+
+    def get_details(self, data_name):
+        dataset_dict = {"ETTh1_M_U": {"input": 6,
+                                      "output": 1,
+                                      "target": "OT",
+                                      "dataset": ETTh1Dataset().load().pd_dataframe()}
+        }
+
+        return dataset_dict[data_name]
+
+
+      
+
+
+    def uni_uni(self, data):
+        X_train, X_test = train_test_split(data, test_size=0.2, shuffle=False)
+        sc = MinMaxScaler()
+        X_train_std = sc.fit_transform(X_train)
+        X_test_std = sc.transform(X_test)
+
+        X_train_std = torch.Tensor(X_train_std)
+        X_test_std = torch.Tensor(X_test_std)
+
+        X_train_std = rearrange(X_train_std, 'b 1 -> b 1 1')
+        X_test_std = rearrange(X_test_std, 'b 1 -> b 1 1')
+
+        X_input = X_train_std[:-1,:,:]
+        y_input = X_train_std[1:, :, :]
+        y_input = rearrange(y_input, 'b 1 1 -> b 1')
+
+        X_test = X_test_std[:-1,:,:]
+        y_test = X_test_std[1:,:,:]
+        y_test = rearrange(y_test, 'b 1 1 -> b 1')
+        return X_input, X_test, y_input, y_test
+    
+    def multi_multi(self, data):
+        X_train, X_test = train_test_split(data, test_size=0.2, shuffle=False)
+        sc = MinMaxScaler()
+        X_train_std = sc.fit_transform(X_train)
+        X_test_std = sc.transform(X_test)
+
+        X_train_std = torch.Tensor(X_train_std)
+        X_test_std = torch.Tensor(X_test_std)
+
+        X_train_std = rearrange(X_train_std, 'b c -> b c 1')
+        X_test_std = rearrange(X_test_std, 'b c -> b c 1')
+
+        X_input = X_train_std[:-1,:,:]
+        y_input = X_train_std[1:, :, :]
+        y_input = rearrange(y_input, 'b c 1 -> b c')
+
+        X_test = X_test_std[:-1,:,:]
+        y_test = X_test_std[1:,:,:]
+        y_test = rearrange(y_test, 'b c 1 -> b c')
+        # print(X_input.shape, X_test.shape, y_input.shape, y_test.shape)
+        return X_input, X_test, y_input, y_test
+
+
+    def multi_uni(self, data):
+        X = data[:,:-1]
+        y = data[:,-1]
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False)
+    
+        sc1 = MinMaxScaler()
+        sc2 = MinMaxScaler()
+    
+        y_train = rearrange(y_train, 'r -> r 1')
+        y_test = rearrange(y_test, 'r -> r 1')
+    
+
+        X_train_std = sc1.fit_transform(X_train)
+        X_test_std = sc1.transform(X_test)
+
+        y_train_std = sc2.fit_transform(y_train)
+        y_test_std = sc2.transform(y_test)
+
+        X_train_std = torch.Tensor(X_train_std)
+        X_test_std = torch.Tensor(X_test_std)
+
+        y_train_std = torch.Tensor(y_train_std)
+        y_test_std = torch.Tensor(y_test_std)
+
+        X_train_std = rearrange(X_train_std, 'b c -> b c 1')
+        X_test_std = rearrange(X_test_std, 'b c -> b c 1')
+
+        return X_train_std, X_test_std, y_train_std, y_test_std
+
+
+    
+            
+
+            
+
--- scripts/layers/esn.py
+++ scripts/layers/esn.py
@@ -32,6 +32,7 @@ class ESN(nn.Module):
         self.state = torch.zeros(self.reservoir_size, 1)
 
         ## Initialize Input Weights as randomly distributed [-1,1].
+        # w = torch.empty(size,size)
         self.W_in = torch.rand((self.reservoir_size, self.input_size)) * 2 -1
 
         ##Initialize Reservoir Weight matrix.
@@ -42,19 +43,14 @@ class ESN(nn.Module):
         self.W_out = None
                              
         ## To capture all states for abalation study.
+        self.plot_states = [self.state]
+
         self.all_states = [self.state]                                    
         
         ## Initialize Feedback weights as gaussian randomly distributed.
         if self.teacher_forcing:
-            self.W_fb = nn.Parameter(torch.rand((self.reservoir_size, self.output_size)), 
-                                     requires_grad=False)
-            
-            self.W = torch.hstack([self.W_in, self.W_res, self.W_fb])
-            
-
-        else:
-
-            self.W = torch.hstack([self.W_in, self.W_res])
+            self.W_fb = torch.rand((self.reservoir_size, self.output_size))
+          
 
 
     @staticmethod
@@ -62,7 +58,9 @@ class ESN(nn.Module):
         ## Initializing Reservoir Weights according to "Re-visiting the echo state property"(2012)
         ##
         ## Initialize a random matrix and induce sparsity.
-
+        
+        # w = torch.empty(size,size)
+        # W_res = nn.init.orthogonal_(w)
         W_res = torch.rand((size, size))
         W_res[torch.rand(*W_res.shape) > connectivity_rate] = 0
 
@@ -81,7 +79,15 @@ class ESN(nn.Module):
         return W_res.to_sparse()
 
 
+    ## Calculate states for all inputs.
     def update_state(self, X, y=None):
+
+        # Change dimension of y from (B, D) to (B, D, 1)
+        y = y.unsqueeze(2)
+
+        # Add initial y value as 0.
+        y = torch.vstack([torch.zeros(1, y.shape[1], y.shape[2]), y])
+        # print(y.shape)
         for i in range(X.shape[0]):
 
             if self.teacher_forcing:
@@ -92,37 +98,41 @@ class ESN(nn.Module):
             
             self.all_states.append(self.state)
 
-
+    ## Calculate Output Weights using ordinary least squares.
     def calc_output_weight(self, y):
         A_matrix = torch.hstack(self.all_states[1:]).T
         B_matrix = y
+        # print(A_matrix.shape, B_matrix.shape)
         self.W_out = torch.linalg.lstsq(A_matrix, B_matrix).solution.T
 
+
+    ## Reset all states for next batch operation.
     def reset_states(self):
         self.state = torch.zeros(self.reservoir_size, 1)
-        self.all_states = []
+        self.all_states = [self.state]  
                     
-    
+    ## Calculate state.
     def get_state(self, input, output=None):
         if self.teacher_forcing:
-            return self.activation(torch.matmul(self.W, torch.vstack([input, self.state])))
+            return self.activation(self.W_in@input + self.W_res@self.state + self.W_fb@output)
         else:
-            return self.activation(torch.matmul(self.W, torch.vstack([input, self.state, output])))
+            return self.activation(self.W_in@input + self.W_res@self.state )
         
-
+    ## Collect last values of state, input and output.
     def collect(self, input, output):
         self.last_state = self.state
         self.last_input = input
         self.last_output = output
     
+
     def forward(self, X, y=None):
         
-
+        ## Perform the below steps only during training to update the output weights.
         if self.training:
-            self.update_state(X, y)
-            self.calc_output_weight(y)
-            self.reset_states()
-            self.collect(X[-1], y[-1])
+            self.update_state(X, y)     # 1. Calculate all states.
+            self.calc_output_weight(y)  # 2. Calculate W_out.
+            self.reset_states()         # 3. Reset batch for next batch.
+            self.collect(X[-1], y[-1].view(y.shape[1], 1))  # 4. Collect last values of last batch to predict future values.
         
         out = [self.last_output]
         
@@ -135,6 +145,7 @@ class ESN(nn.Module):
             else:
                 self.state = self.get_state(X[i])
 
+            self.plot_states.append(self.state)
             pred = torch.matmul(self.W_out, self.state)
             out.append(pred)
 
