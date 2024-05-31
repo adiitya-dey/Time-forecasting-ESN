b8040f8d71500fdcb2971d71b2a873df0d957100
aditya dey <adiitya.dey@gmail.com>
2024-05-31 16:28:39 +0200

    removed Airpassenger and AusBeer

--- scripts/data/data_loader.py
+++ scripts/data/data_loader.py
@@ -1,7 +1,5 @@
 
-from darts.datasets import AirPassengersDataset, AusBeerDataset,\
-      ETTh1Dataset, ETTh2Dataset, ETTm1Dataset, ETTm2Dataset, \
-          ExchangeRateDataset, TrafficDataset, WeatherDataset
+from darts.datasets import ETTh1Dataset, ETTh2Dataset, ETTm1Dataset, ETTm2Dataset,ExchangeRateDataset, TrafficDataset, WeatherDataset
 
 import numpy as np
 import torch
@@ -69,7 +67,11 @@ class DartsDataset:
                         "Weather_M_U": {"input": 20,
                                       "output": 1,
                                       "target": "T (degC)",
-                                      "dataset": WeatherDataset().load().pd_dataframe()}           
+                                      "dataset": WeatherDataset().load().pd_dataframe()},
+                        "ExRate_U": {"input": 1,
+                                      "output": 1,
+                                      "target": "7",
+                                      "dataset": ExchangeRateDataset().load().pd_dataframe()},                        
         }
 
         return dataset_dict[data_name]
