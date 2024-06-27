4a3db6ce02ed542250c89faf04e10a258f13ad04
aditya dey <adiitya.dey@gmail.com>
2024-06-27 12:55:52 +0200

    checkpoints are necessary

--- ESNLinear/exp/exp_main.py
+++ ESNLinear/exp/exp_main.py
@@ -53,31 +53,9 @@ class Exp_Main(Exp_Basic):
                 batch_x = batch_x.float().to(self.device)
                 batch_y = batch_y.float()
 
-                # batch_x_mark = batch_x_mark.float().to(self.device)
-                # batch_y_mark = batch_y_mark.float().to(self.device)
-
-                # # decoder input
-                # dec_inp = torch.zeros_like(batch_y[:, -self.args.pred_len:, :]).float()
-                # dec_inp = torch.cat([batch_y[:, :self.args.label_len, :], dec_inp], dim=1).float().to(self.device)
-                # encoder - decoder
-                # if self.args.use_amp:
-                #     with torch.cuda.amp.autocast():
-                #         if 'ESNLinear' in self.args.model:
-                #             outputs = self.model(batch_x)
-                #         else:
-                #             if self.args.output_attention:
-                #                 outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0]
-                #             else:
-                #                 outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
-                # else:
-                #     if 'ESNLinear' in self.args.model:
+              
                 outputs = self.model(batch_x)
-                    # else:
-                    #     if self.args.output_attention:
-                    #         outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0]
-                    #     else:
-                    #         outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
-                    
+                   
 
                 f_dim = -1 if self.args.features == 'MS' else 0
                 outputs = outputs[:, -self.args.pred_len:, f_dim:]
@@ -271,61 +249,7 @@ class Exp_Main(Exp_Basic):
         f.close()
 
         # np.save(folder_path + 'metrics.npy', np.array([mae, mse, rmse, mape, mspe,rse, corr]))
-        np.save(folder_path + 'pred.npy', preds)
+        # np.save(folder_path + 'pred.npy', preds)
         # np.save(folder_path + 'true.npy', trues)
         # np.save(folder_path + 'x.npy', inputx)
-        return
-
-    def predict(self, setting, load=False):
-        pred_data, pred_loader = self._get_data(flag='pred')
-
-        if load:
-            path = os.path.join(self.args.checkpoints, setting)
-            best_model_path = path + '/' + 'checkpoint.pth'
-            self.model.load_state_dict(torch.load(best_model_path))
-
-        preds = []
-
-        self.model.eval()
-        with torch.no_grad():
-            for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in enumerate(pred_loader):
-                batch_x = batch_x.float().to(self.device)
-                batch_y = batch_y.float()
-                batch_x_mark = batch_x_mark.float().to(self.device)
-                batch_y_mark = batch_y_mark.float().to(self.device)
-
-                # decoder input
-                dec_inp = torch.zeros([batch_y.shape[0], self.args.pred_len, batch_y.shape[2]]).float().to(batch_y.device)
-                dec_inp = torch.cat([batch_y[:, :self.args.label_len, :], dec_inp], dim=1).float().to(self.device)
-                # encoder - decoder
-                if self.args.use_amp:
-                    with torch.cuda.amp.autocast():
-                        if 'ESNLinear' in self.args.model:
-                            outputs = self.model(batch_x)
-                        else:
-                            if self.args.output_attention:
-                                outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0]
-                            else:
-                                outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
-                else:
-                    if 'ESNLinear' in self.args.model:
-                        outputs = self.model(batch_x)
-                    else:
-                        if self.args.output_attention:
-                            outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0]
-                        else:
-                            outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
-                pred = outputs.detach().cpu().numpy()  # .squeeze()
-                preds.append(pred)
-
-        preds = np.array(preds)
-        preds = preds.reshape(-1, preds.shape[-2], preds.shape[-1])
-
-        # result save
-        folder_path = './results/' + setting + '/'
-        if not os.path.exists(folder_path):
-            os.makedirs(folder_path)
-
-        np.save(folder_path + 'real_prediction.npy', preds)
-
         return
