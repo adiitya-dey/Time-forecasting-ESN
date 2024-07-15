fcd213aa72e5d6502eddd4ea6a11c46202bf4d18
aditya dey <adiitya.dey@gmail.com>
2024-07-15 14:46:42 +0200

    ESN + output + projection works successfully with 0.728 MSE. (Checkpoint1)

--- ESNLinear/models/ESNLinear.py
+++ ESNLinear/models/ESNLinear.py
@@ -3,39 +3,41 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
-# from ..layers.Embed import PositionalEmbedding
 
-class moving_avg(nn.Module):
-    """
-    Moving average block to highlight the trend of time series
-    """
-    def __init__(self, kernel_size, stride):
-        super(moving_avg, self).__init__()
-        self.kernel_size = kernel_size
-        self.avg = nn.AvgPool1d(kernel_size=kernel_size, stride=stride, padding=0)
+class PrimaryNetwork(nn.Module):
 
-    def forward(self, x):
-        # padding on the both ends of time series
-        front = x[:, 0:1, :].repeat(1, (self.kernel_size - 1) // 2, 1)
-        end = x[:, -1:, :].repeat(1, (self.kernel_size - 1) // 2, 1)
-        x = torch.cat([front, x, end], dim=1)
-        x = self.avg(x.permute(0, 2, 1))
-        x = x.permute(0, 2, 1)
-        return x
+    def __init__(self, in_features: int, out_features: int) -> None:
+        super(PrimaryNetwork, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
 
+        self.fc = nn.Linear(in_features=self.in_features,
+                            out_features=self.out_features,
+                            bias=True)
+        
+    def forward(self, x, weights, bias):
+            self.fc.weight = nn.Parameter(weights)
+            self.fc.bias = nn.Parameter(bias)
+            return self.fc(x)
+    
+class HyperNetwork(nn.Module):
 
-class series_decomp(nn.Module):
-    """
-    Series decomposition block
-    """
-    def __init__(self, kernel_size):
-        super(series_decomp, self).__init__()
-        self.moving_avg = moving_avg(kernel_size, stride=1)
+    def __init__(self, h_size, primary_input_size, primary_output_size) -> None:
+        super(HyperNetwork, self).__init__()
+        self.primary_input_size = primary_input_size
+        self.primary_output_size = primary_output_size
+        self.weight_gen = nn.Linear(h_size, self.primary_input_size * self.primary_output_size)
+        self.bias_gen = nn.Linear(h_size, self.primary_output_size)
 
-    def forward(self, x):
-        moving_mean = self.moving_avg(x)
-        res = x - moving_mean
-        return res, moving_mean
+    def forward(self, h):
+        weights = self.weight_gen(h)
+        bias = self.bias_gen(h)
+        
+        ## Reshape weights to match primary network's weight shape
+        weights = weights.view(-1, self.primary_output_size, self.primary_input_size)
+        return weights, bias
+    
+
 
 
 class ESN(nn.Module):
@@ -116,7 +118,7 @@ class Model(nn.Module):
         self.seq_len = configs.seq_len
         self.pred_len = configs.pred_len
         self.window_len = 12
-        self.reservoir_size = 5
+        self.reservoir_size = 25
         self.washout = 10
 
 
@@ -124,8 +126,8 @@ class Model(nn.Module):
         self.pred_seg = self.pred_len // self.window_len
 
         # Decompsition Kernel Size
-        kernel_size = 25
-        self.decompsition = series_decomp(kernel_size)
+        # kernel_size = 25
+        # self.decompsition = series_decomp(kernel_size)
         self.individual = configs.individual
         self.channels = configs.enc_in
 
@@ -139,12 +141,12 @@ class Model(nn.Module):
 
         self.output_layer1 = nn.Linear(in_features=(self.input_seg - self.washout)*self.reservoir_size,
                                                  out_features=self.pred_seg*self.reservoir_size,
-                                                 bias=False)
+                                                 bias=True)
         # self.dropout = nn.Dropout(0.2)
 
         self.projection = nn.Linear(in_features=self.reservoir_size,
                                     out_features=self.window_len,
-                                    bias=False)
+                                    bias=True)
             
        
     def forward(self, x):
